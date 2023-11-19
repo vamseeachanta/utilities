96261af233cca794c0690b966fd8d452dd3de363
Vamsee Achanta <achantav@gmail.com>
2023-11-19 07:13:13 -0600

    polar_wip

--- src/assetutilities/common/visualization_components.py
+++ src/assetutilities/common/visualization_components.py
@@ -67,6 +67,13 @@ class VisualizationComponents():
 
         return data_dict
 
+    def get_axis_for_polar(self, rect):
+        rect_polar = rect.copy()
+        rect_polar[0] = rect[0] * np.pi / 180
+        rect_polar[1] = rect[1] * np.pi / 180
+
+        return rect_polar
+
     def get_raw_data(self):
         if self.cfg.default['input_data']['source'] == 'db':
             self.get_environments()
@@ -336,20 +343,32 @@ class VisualizationComponents():
             plt = plt_settings['plt_properties']['plt']
 
         # Add axis for plot
+
+        alpha = plt_settings.get('alpha', 1)
+        facecolor = plt_settings.get('facecolor', None)
         if (not 'add_axes' in plt_settings) or (not plt_settings['add_axes']):
-            fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+            fig, ax = plt.subplots(subplot_kw={'projection': 'polar'},
+                                   facecolor=facecolor,
+                                   alpha=alpha)
         else:
-            fig = plt_settings['fig']
-            ax = fig.add_axes([0.2, 0.47, 0.30, 0.40],
+            fig = plt_settings['plt_properties']['fig']
+            rect = plt_settings['rect']
+            ax = fig.add_axes(rect,
                               polar=True,
-                              facecolor='yellow')
+                              facecolor=facecolor,
+                              alpha=alpha)
+
+            axis = plt_settings['axis']
+            if axis != 'off':
+                axis = self.get_axis_for_polar(axis)
+            plt.axis(axis)
 
         # Add trace or plot style
         if plt_settings['plt_kind'] == 'polar':
             for index in range(0, plt_settings['traces']):
                 ax.plot(df['theta_' + str(index)],
                         df['r_' + str(index)],
-                        label=cfg['data']['legend'][index],
+                        label=plt_settings['legend']['label'][index],
                         color=cfg['data']['color'][index],
                         linestyle=cfg['data']['linestyle'][index],
                         alpha=cfg['data']['alpha'][index])
@@ -357,13 +376,17 @@ class VisualizationComponents():
             for index in range(0, plt_settings['traces']):
                 ax.scatter(df['theta_' + str(index)],
                            df['r_' + str(index)],
-                           label=cfg['data']['legend']['label'][index],
+                           label=plt_settings['legend']['label'][index],
                            color=cfg['data']['color'][index],
                            linestyle=cfg['data']['linestyle'][index],
                            alpha=cfg['data']['alpha'][index])
 
-        if plt_settings['traces'] > 1:
+        legend_flag = plt_settings['legend'].get('flag', True)
+        if legend_flag:
             ax.legend(loc='best')
+            prop = plt_settings['legend'].get('prop', None)
+            if prop is not None:
+                plt.legend(prop=prop)
             # ax.legend(bbox_to_anchor=(1.04, 0.5),
             #           loc="center left",
             #           borderaxespad=0)
@@ -372,19 +395,19 @@ class VisualizationComponents():
 
         plt = self.get_plt_with_arrows(plt, plt_settings)
 
-        set_rmax = plt_settings['set_rmax']
+        set_rmax = plt_settings.get('set_rmax', None)
         if set_rmax is not None:
             ax.set_rmax(set_rmax)
 
-        set_rticks = plt_settings['set_rticks']
+        set_rticks = plt_settings.get('set_rticks', None)
         if set_rticks is not None:
             ax.set_rticks(set_rticks)
 
-        set_rlabel_position = plt_settings['set_rlabel_position']
+        set_rlabel_position = plt_settings.get('set_rlabel_position', None)
         if set_rlabel_position is not None:
             ax.set_rlabel_position(set_rlabel_position)
 
-        set_thetagrids = plt_settings['set_thetagrids']
+        set_thetagrids = plt_settings.get('set_thetagrids', None)
         if set_thetagrids is not None:
             ax.set_thetagrids(set_thetagrids)
 
@@ -393,8 +416,12 @@ class VisualizationComponents():
         if set_theta_zero_location is not None:
             ax.set_theta_zero_location(set_theta_zero_location)
 
-        ax.grid(True)
-        ax.set_title(plt_settings['title'], va='bottom')
+        grid = plt_settings.get('grid', True)
+        ax.grid(grid)
+
+        title = plt_settings.get('title', None)
+        if title is not None:
+            ax.set_title(plt_settings['title'], va='bottom')
 
         plt_properties = {'plt': plt, 'fig': fig}
         if 'add_axes' in cfg and len(cfg.add_axes) > 0:
@@ -406,8 +433,12 @@ class VisualizationComponents():
         for axes_idx in range(0, len(cfg.add_axes)):
             cfg_plt = cfg.add_axes[axes_idx]
             plt_settings = cfg_plt['settings']
+            plt_settings.update({'add_axes': True})
             plt_settings.update({'plt_properties': plt_properties})
+
             data_df = self.get_polar_data(cfg_plt)
+            plt_settings['traces'] = int(len(data_df.columns) / 2)
+
             plt = self.get_polar_plot_matplotlib(data_df, plt_settings, cfg_plt)
 
     def get_plt_with_arrows(self, plt, plt_settings):
---
+++ src/assetutilities/tests/visualizations_tests_temp.bat
@@ -0,0 +1,10 @@
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_pltoly.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib.yml
+
+REM Project Specific files
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_ini_step6.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_normal_lay.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_deck_handling.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_laydown.yml
+
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes.yml
