ade0c2a28529b7526644e24f6e4f7d4b30e264b3
Vamsee Achanta <achantav@gmail.com>
2023-11-20 15:52:37 -0600

    wip

--- src/assetutilities/tests/visualizations_tests_temp.bat
+++ src/assetutilities/tests/visualizations_tests_temp.bat
@@ -1,17 +1,16 @@
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_pltoly.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_pltoly.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib.yml
 
 REM Project Specific files
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_ini_step6.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_normal_lay.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_deck_handling.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_laydown.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_ini_step6.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_normal_lay.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_deck_handling.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_midsplice_laydown.yml
 
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes.yml
 
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_ini.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_deck_handling.yml
-REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_laydown.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_ini.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_deck_handling.yml
 CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_laydown.yml
-
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_laydown.yml
 CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_normal_lay.yml
