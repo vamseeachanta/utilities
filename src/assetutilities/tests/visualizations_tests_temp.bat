97cc7db0d0dff8b5dec7ab8cccb3888b5eb31c74
Vamsee Achanta <achantav@gmail.com>
2023-11-20 14:38:54 -0600

    wip

--- src/assetutilities/tests/visualizations_tests_temp.bat
+++ src/assetutilities/tests/visualizations_tests_temp.bat
@@ -9,4 +9,9 @@ REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_
 
 REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes.yml
 
-CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_ini.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_ini.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_deck_handling.yml
+REM CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_laydown.yml
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_midlinesplice_laydown.yml
+
+CALL python -m assetutilities .\test_data\visualization\visualization_polar_matplotlib_add_axes_normal_lay.yml
