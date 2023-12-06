20401aa826195622a8a3c19f335ab4cff9cd9ac7
Vamsee Achanta <achantav@gmail.com>
2023-12-06 14:52:38 -0600

    wip

--- src/assetutilities/tools/git/daily_routine_repos.bat
+++ src/assetutilities/tools/git/daily_routine_repos.bat
@@ -1,94 +1,94 @@
 REM #TODO get current directory and then refer back to the daily_routine batch file relative to this directory.
-cd C:\Users\ss7a2365\Documents\github\structuralstrength
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\structuralstrength
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\aceengineer-admin
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\aceengineer-admin
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\aceengineercode
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\aceengineercode
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\achantas-data
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\achantas-data
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\achantas-media
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\achantas-media
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\assetutilities
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\assetutilities
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\client_projects
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\client_projects
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\digitalmodel
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\digitalmodel
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\digitaltwinfeed
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\digitaltwinfeed
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\ecs
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\ecs
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\energy
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\energy
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\energy_analysis
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\energy_analysis
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\energydata
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\energydata
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\hobbies
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\hobbies
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\Internal-Discussions
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\Internal-Discussions
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\investments
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\investments
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\leveraged-multi-family-analysis
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\leveraged-multi-family-analysis
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-REM cd C:\Users\ss7a2365\Documents\github\markdown-resume
-REM cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+REM cd C:\Users\user.name\Documents\github\markdown-resume
+REM cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\OGManufacturing
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\OGManufacturing
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\petrope
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\petrope
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\pipecapacity
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\pipecapacity
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\predyct
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\predyct
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\probabilisticrisk
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\probabilisticrisk
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\py_package
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\py_package
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\pyintegrity
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\pyintegrity
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\python-practice
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\python-practice
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\rock-oil-field
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\rock-oil-field
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\sd-work
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\sd-work
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\spire_projects
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\spire_projects
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\stockhold
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\stockhold
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
-cd C:\Users\ss7a2365\Documents\github\teamresumes
-cmd /c "C:\Users\ss7a2365\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
+cd C:\Users\user.name\Documents\github\teamresumes
+cmd /c "C:\Users\user.name\Documents\github\assetutilities\src\assetutilities\tools\git\daily_routine.bat"
 
