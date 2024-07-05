2e3028abe17f1144314cddc36f711b8925861ede
Vamsee Achanta <achantav@gmail.com>
2024-07-05 11:32:43 -0500

    wip

--- src/assetutilities/tests/test_excel_utilities_excel_to_image.py
+++ src/assetutilities/tests/test_excel_utilities_excel_to_image.py
@@ -3,15 +3,19 @@ Not worked as intended. Difficult to work between Excel workbooks to keep live l
 Easier to just do a brute force copy and paste and rerun script if needed.
 """
 
-import pytest
-import deepdiff
+# Standard library imports
 import os
 import sys
 
+# Third party imports
+import pytest
+
+
+# Reader imports
 from assetutilities.engine import engine
-from assetutilities.common.yml_utilities import ymlInput
 
 
+@pytest.mark.skip(reason="Only works in local drive")
 def run_excel_utilties_closed_file_reference(input_file, expected_result={}):
     if input_file is not None and not os.path.isfile(input_file):
         input_file = os.path.join(os.path.dirname(__file__), input_file)
@@ -26,12 +30,13 @@ def run_excel_utilties_closed_file_reference(input_file, expected_result={}):
     #                              significant_digits=4)
 
 
+@pytest.mark.skip(reason="Only works in local drive")
 def get_valid_pytest_output_file(pytest_output_file):
     if pytest_output_file is not None and not os.path.isfile(pytest_output_file):
         pytest_output_file = os.path.join(os.path.dirname(__file__), pytest_output_file)
     return pytest_output_file
 
-
+@pytest.mark.skip(reason="Only works in local drive")
 def test_excel_utilties_closed_file_reference():
     input_file = "test_data/excel_utilities/excel_utilities_excel_to_image.yml"
 
