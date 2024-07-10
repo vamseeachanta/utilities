7c184cc0c8af7093526f5bf1a34146365838a776
Vamsee Achanta <achantav@gmail.com>
2024-07-09 23:15:08 -0500

    20240907

---
+++ src/assetutilities/common/visualization/visualization_templates.py
@@ -0,0 +1,23 @@
+# Reader imports
+from assetutilities.common.data import AttributeDict
+from assetutilities.common.yml_utilities import WorkingWithYAML
+
+wwy = WorkingWithYAML()
+
+
+class VisualizationTemplates:
+
+    def __init__(self):
+        pass
+
+    def get_xy_plot(self, custom_analysis_dict={}):
+        library_name = 'assetutilities'
+        library_yaml_cfg = {
+            'filename': 'tests/test_data/visualization/xy_line_matplotlib_input.yml',
+            'library_name': library_name
+        }
+        plot_template = wwy.get_library_yaml_file(library_yaml_cfg)
+        plot_template['Analysis'] = custom_analysis_dict
+        plot_template = AttributeDict(plot_template)
+
+        return plot_template
