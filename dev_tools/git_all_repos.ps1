9b8bb01eb340ce71c8017f526e1ea05bbfdb4ca2
Vamsee Achanta <achantav@gmail.com>
2024-07-10 15:13:10 -0500

    20241007

--- dev_tools/git_all_repos.ps1
+++ dev_tools/git_all_repos.ps1
@@ -3,7 +3,16 @@
 $dir = dir C:\github | ?{$_.PSISContainer}
 
 foreach ($d in $dir){
-    $daily_routine_batch = Join-Path -Path $d.FullName -ChildPath ("\dev_tools\daily_routine.bat")
+    echo "Running git push pull in folder      ... START"
+    echo $d.FullName
+    Set-Location -Path $d.FullName
+    $daily_routine_batch = Join-Path $d.FullName -ChildPath ("\dev_tools\daily_routine.bat")
     
     & $daily_routine_batch
+    & echo "Running git push pull in folder      ... DONE"
+    & echo ""
+    & echo ""
 }
+
+Set-Location -Path $dir
+ 
