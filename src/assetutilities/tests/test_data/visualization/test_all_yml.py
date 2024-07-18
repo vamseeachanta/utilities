18276b129507869bef57653328bfefa61bb43c97
Vamsee Achanta <achantav@gmail.com>
2024-07-18 12:02:51 -0500

    wip

--- src/assetutilities/tests/test_data/visualization/test_all_yml.py
+++ src/assetutilities/tests/test_data/visualization/test_all_yml.py
@@ -1,5 +1,12 @@
 #TODO 
 
-read all yml files and run all the files using the below command
+# read all yml files in the current directory
+# Loop through each yml file and run the below command
+  # Hint1: python subprocess;  (Esier route). 
+  # Hint2: subprocess separate command prompt. You have open a minconda prompt and ensure same python enviornment is used. to run each yml file
+# If it ran, get success or failure. If one yml file fails.. then fail the test else the test passes
+    # save a csv with all success and failures.
+
+
 
 python -m assetutilities visualization_polar_ini_step6.yml
