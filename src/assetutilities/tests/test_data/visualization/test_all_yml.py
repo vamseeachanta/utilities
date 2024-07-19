d3313fd61a24724d6df5bd6c3021d614c0f005e6
samdansk2 <samdanshaik8998@gmail.com>
2024-07-19 15:36:19 +0530

    refactor

--- src/assetutilities/tests/test_data/visualization/test_all_yml.py
+++ src/assetutilities/tests/test_data/visualization/test_all_yml.py
@@ -10,32 +10,28 @@ import subprocess
 import pandas as pd
 
 def run_yaml_files(directory):
-    # Initialize lists to store the results
+    
     filenames = []
     statuses = []
 
-    # Loop through all YAML files in the directory
     for filename in os.listdir(directory):
         if filename.endswith('.yml') or filename.endswith('.yaml'):
             file_path = os.path.join(directory, filename)
             try:
-                # Run the command
                 result = subprocess.run(['python', '-m', 'assetutilities', file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                print(result.stdout.decode())  # Print the command output
+                print(result.stdout.decode())  
                 filenames.append(filename)
                 statuses.append('Success')
             except subprocess.CalledProcessError as e:
-                print(e.stderr.decode())  # Print the error output
+                print(e.stderr.decode())  
                 filenames.append(filename)
                 statuses.append('Failed')
 
-    # Create a DataFrame and save to CSV
     df = pd.DataFrame({'Filename': filenames, 'Status': statuses})
     output_csv = os.path.join(directory, 'results.csv')
     df.to_csv(output_csv, index=False)
     print(f'Results saved to {output_csv}')
 
-# Usage example:
 directory = r'src\assetutilities\tests\test_data\visualization'
 run_yaml_files(directory)
 
