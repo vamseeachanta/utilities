898188fcb050ca9aaf164d37c52ef987f94df894
Vamsee Achanta <achantav@gmail.com>
2024-07-24 11:16:35 -0500

    wip

--- src/assetutilities/tests/test_data/test_all_yml.py
+++ src/assetutilities/tests/test_data/test_all_yml.py
@@ -1,8 +1,10 @@
+# Standard library imports
 import os
-import subprocess
+
+# Third party imports
 import pandas as pd
 
-def run_yaml_files(root_directory, subfolders):
+
     
     folders = []
     filenames = []
@@ -16,7 +18,7 @@ def run_yaml_files(root_directory, subfolders):
                 if filename.endswith(('.yml', '.yaml')) and not filename.lower().startswith('app'):
                     file_path = os.path.join(subfolder_path, filename)
                     try:
-                        result = subprocess.run(['python', '-m', 'assetutilities', file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        result = subprocess.run(['python', '-m', library, file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                         print(result.stdout.decode()) 
                         folders.append(subfolder)
                         filenames.append(filename)
@@ -34,6 +36,15 @@ def run_yaml_files(root_directory, subfolders):
             output_csv = os.path.join(root_directory, 'file_status.csv')
             df.to_csv(output_csv, index=False)
 
-root_directory = r'src\assetutilities\tests\test_data'  
-subfolders = ['data_exploration','edit_pdf','excel_utilities','file_edit','file_management','read_pdf','visualization','word_utilities','yml']  
-run_yaml_files(root_directory, subfolders)
+    print(f'Detailed output: {df}')
+    print(f'No. of files processed: {len(filenames)}')
+    print("Tests passed: ", len(df[df['Status'] == 'Success']))
+    print("Tests failed: ", len(df[df['Status'] == 'Failed']))
+    print('Done!')
+
+
+if __name__ == '__main__':
+    library = 'assetutilities'
+    root_directory = f'src/{library}/tests/test_data' 
+    subfolders = ['data_exploration','edit_pdf','excel_utilities','file_edit','file_management','read_pdf','visualization','word_utilities','yml']  
+    run_yaml_files(root_directory, subfolders)
