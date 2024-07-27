8e69d831cebcf394904ac126ce933949ce6fa38e
samdansk2 <samdanshaik8998@gmail.com>
2024-07-27 12:19:06 +0530

    Refactor

--- src/assetutilities/common/word_utilities.py
+++ src/assetutilities/common/word_utilities.py
@@ -34,34 +34,37 @@ class WordUtilities:
         # Search returns true if found
 
     def csv_to_docx(self, cfg):
+        
         input_csv = cfg['files']['file_name']
 
-        # Read the CSV file as DataFrame
         df = pd.read_csv(input_csv)
 
-        # Create a new Document
+        for col in df.select_dtypes(include=['float']).columns:
+            df[col] = df[col].round(2)
+            
         doc = Document()
 
-        # Add a table to the Document
         table = doc.add_table(rows=1, cols=len(df.columns))
         table.style = 'Table Grid'
 
-        # Format the header row
         hdr_cells = table.rows[0].cells
         for i, column_name in enumerate(df.columns):
             hdr_cells[i].text = column_name
-            hdr_cells[i].paragraphs[0].runs[0].font.size = Pt(10)
-            hdr_cells[i].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+            for paragraph in hdr_cells[i].paragraphs:
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+                for run in paragraph.runs:
+                    run.font.size = Pt(10)
 
-        # Add the rows to the table
         for index, row in df.iterrows():
             row_cells = table.add_row().cells
             for i, cell in enumerate(row):
                 row_cells[i].text = str(cell)
-                row_cells[i].paragraphs[0].runs[0].font.size = Pt(10)
-                row_cells[i].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+                for paragraph in row_cells[i].paragraphs:
+                    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+                    for run in paragraph.runs:
+                        run.font.size = Pt(10)
 
-        # Save the document
         output_docx = input_csv.replace(".csv", ".docx")
         doc.save(output_docx)
+        
         print(f"CSV file has been converted to DOCX file ")
