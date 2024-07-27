d2a9ee009821c0e06e6343f6925ae90b2d3fe017
samdansk2 <samdanshaik8998@gmail.com>
2024-07-27 11:03:07 +0530

    csv to word

--- src/assetutilities/common/word_utilities.py
+++ src/assetutilities/common/word_utilities.py
@@ -1,16 +1,14 @@
-# Standard library imports
-
-# Standard library imports
 from re import search
 
-# Third party imports
-from docx import Document, opendocx
+import docx
 
-# from docx import opendocx
-
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+import pandas as pd
+import os
 
 class WordUtilities:
-
     def __init__(self):
         pass
 
@@ -31,11 +29,39 @@ class WordUtilities:
                 print(True)
 
         # Open the .docx file
-        document = opendocx("A document.docx")
+        #document = opendocx("A document.docx")
 
         # Search returns true if found
-        search(document, "your search string")
 
     def csv_to_docx(self, cfg):
-        pass
-        # CODE GOES HERE
+        input_csv = cfg['files']['file_name']
+
+        # Read the CSV file as DataFrame
+        df = pd.read_csv(input_csv)
+
+        # Create a new Document
+        doc = Document()
+
+        # Add a table to the Document
+        table = doc.add_table(rows=1, cols=len(df.columns))
+        table.style = 'Table Grid'
+
+        # Format the header row
+        hdr_cells = table.rows[0].cells
+        for i, column_name in enumerate(df.columns):
+            hdr_cells[i].text = column_name
+            hdr_cells[i].paragraphs[0].runs[0].font.size = Pt(10)
+            hdr_cells[i].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+        # Add the rows to the table
+        for index, row in df.iterrows():
+            row_cells = table.add_row().cells
+            for i, cell in enumerate(row):
+                row_cells[i].text = str(cell)
+                row_cells[i].paragraphs[0].runs[0].font.size = Pt(10)
+                row_cells[i].paragraphs[0].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+        # Save the document
+        output_docx = input_csv.replace(".csv", ".docx")
+        doc.save(output_docx)
+        print(f"CSV file has been converted to DOCX file ")
