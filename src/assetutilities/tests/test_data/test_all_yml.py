840c1c18bfedba3cdcd5aad60bbde66bdf5bc1ee
samdansk2 <samdanshaik8998@gmail.com>
2024-08-29 10:25:51 +0530

    improve test yml

--- src/assetutilities/tests/test_data/test_all_yml.py
+++ src/assetutilities/tests/test_data/test_all_yml.py
@@ -9,37 +9,40 @@ from colorama import init as colorama_init
 
 colorama_init()
 
-def run_yaml_files(root_directory, subfolders):
+def run_yaml_files(root_directory):
     
     folders = []
     filenames = []
     statuses = []
 
-    for subfolder in subfolders:
-        subfolder_path = os.path.join(root_directory, subfolder)
-        if os.path.exists(subfolder_path) and os.path.isdir(subfolder_path):
-            
-            for filename in os.listdir(subfolder_path):
-                if filename.endswith(('.yml', '.yaml')) and not filename.lower().startswith('app'):
-                    file_path = os.path.join(subfolder_path, filename)
-                    try:
-                        result = subprocess.run(['python', '-m', library, file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        print(result.stdout.decode()) 
-                        folders.append(subfolder)
-                        filenames.append(filename)
-                        statuses.append('Success')
-                    except subprocess.CalledProcessError as e:
-                        print(e.stderr.decode())  
-                        folders.append(subfolder)
-                        filenames.append(filename)
-                        statuses.append('Failed')
+    for root, dirs, files in os.walk(root_directory):
+        
+        # Skip files in the root directory
+        if root == root_directory:
+            continue
+        
+        for filename in files:
 
-            df = pd.DataFrame({'Folder': folders, 'Filename': filenames, 'Status': statuses})
+            if filename.endswith(('.yml', '.yaml')) and not filename.lower().startswith('app'):
+                file_path = os.path.join(root, filename)
+                try:
+                    result = subprocess.run(['python', '-m', 'assetutilities', file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    print(result.stdout.decode())
+                    folders.append(os.path.relpath(root, root_directory))  
+                    filenames.append(filename)
+                    statuses.append('Success')
+                except subprocess.CalledProcessError as e:
+                    print(e.stderr.decode())  
+                    folders.append(os.path.relpath(root, root_directory)) 
+                    filenames.append(filename)
+                    statuses.append('Failed')
 
-            df = df.sort_values(by=['Status', 'Folder'], ascending=[False, True])
+        df = pd.DataFrame({'Folder': folders, 'Filename': filenames, 'Status': statuses})
 
-            output_csv = os.path.join(root_directory, 'file_status.csv')
-            df.to_csv(output_csv, index=False)
+        df = df.sort_values(by=['Status', 'Folder'], ascending=[False, True])
+
+        output_csv = os.path.join(root_directory, 'file_status.csv')
+        df.to_csv(output_csv, index=False)
 
 
     print(f'Detailed output: {df}')
@@ -51,6 +54,5 @@ def run_yaml_files(root_directory, subfolders):
 
 if __name__ == '__main__':
     library = 'assetutilities'
-    root_directory = f'src/{library}/tests/test_data' 
-    subfolders = ['data_exploration','edit_pdf','excel_utilities','file_edit','file_management','read_pdf','visualization','word_utilities','yml']  
-    run_yaml_files(root_directory, subfolders)
+    root_directory = f'src/{library}/tests/test_data'   
+    run_yaml_files(root_directory)
