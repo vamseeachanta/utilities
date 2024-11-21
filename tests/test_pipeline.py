9cb8a0dc6078f12ffc8fa4b094fd4ac654ecb482
Vamsee Achanta <achantav@gmail.com>
2024-11-21 10:30:42 -0600

    wip

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,12 +1,13 @@
 #import os
 
+
 def test_pass_pipeline():
 
     #library = 'assetutilities'
     root_directory = 'tests'
     summary_file = 'tests/yml_summary.txt'
 
-    
+
     with open(summary_file, 'r') as file:
         content = file.read()
 
@@ -17,6 +18,20 @@ def test_pass_pipeline():
             break
 
     expected_result = 28
+
+
+    summary_file_pytest = 'tests/yml_summary_pytest.txt'
+
+    with open(summary_file_pytest, 'r') as file:
+        content = file.read()
+
+    tests_passed = None
+    for line in content.splitlines():
+        if "Tests passed:" in line:
+            expected_result = int(line.split(":")[1].strip())
+            break
+
+    # expected_result = 28
 
     assert tests_passed == expected_result
 
