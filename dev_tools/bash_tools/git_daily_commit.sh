ade9824b09283c1b62d455c6c2215b6041f705dd
Vamsee Achanta <achantav@gmail.com>
2025-01-15 18:41:28 -0600

    wip

--- dev_tools/bash_tools/git_daily_commit.sh
+++ dev_tools/bash_tools/git_daily_commit.sh
@@ -10,24 +10,28 @@ bash_tools_home="dev_tools/bash_tools"
 source ${bash_tools_home}/common.sh
 # source ${bash_tools_home}/git_select_year_month_branch.sh
 
-today=$(date '+%Y%m%d')
-
+daily_commit_message=$(date '+%Y%m%d')
 
 cat << COM
 Starting daily git routine. key details 
   - Repository name: $repo_name
   - Repository root: $repo_root
-  - Commit date: $today
+  - Daily Commit message: $daily_commit_message
 Executing git operations now 
 COM
 
-# get to repo root
-cd "$repo_root"
+if [ -n "$(git status --porcelain)" ]; then
+    log_message "yellow" "Changes detected in repo: $(basename "$dir")"
 
-# perform git operations
-git pull
-git add --all
-git commit -m "$today"
-git push
+    # get to repo root
+    cd "$repo_root"
+
+    # perform git operations
+    git pull
+    git add --all
+    git commit -m "$daily_commit_message"
+    git push
+
+fi
 
 log_message "green" "Repo : ${repo_name} : Daily git operations completed"
